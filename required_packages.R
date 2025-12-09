299222d99c02886f1af2c8cfcb55d5fc3bf6a8a3
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2025-12-09 09:40:44 -0500

    Add files via upload

--- required_packages.R
+++ required_packages.R
@@ -4,6 +4,8 @@ install.packages("readr")
 install.packages("here")
 install.packages("sjPlot")
 install.packages("lavaan")
+install.packages("gt")
+install.packages("e1071")
 
 # install GitHub packages
 install.packages("devtools")
