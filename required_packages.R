43609daeba8223cc79f93db743fd7dd1bded0c91
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2025-12-09 08:21:25 -0500

    Add files via upload

---
+++ required_packages.R
@@ -0,0 +1,10 @@
+# install from CRAN
+install.packages("dplyr")
+install.packages("readr")
+install.packages("here")
+install.packages("sjPlot")
+install.packages("lavaan")
+
+# install GitHub packages
+install.packages("devtools")
+devtools::install_github("dr-JT/semoutput")
